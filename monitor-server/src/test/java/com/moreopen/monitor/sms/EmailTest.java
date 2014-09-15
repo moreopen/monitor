8df27018ed288c1a8e905c0557cf27c893800529
yekai2 <yekai2@lenovo.com>
2014-09-15 15:33:37 +0800

    commit

--- monitor-server/src/test/java/com/moreopen/monitor/sms/EmailTest.java
+++ monitor-server/src/test/java/com/moreopen/monitor/sms/EmailTest.java
@@ -1,35 +1,28 @@
 package com.moreopen.monitor.sms;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.moreopen.monitor.server.service.alarm.EmailServiceImpl;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:/applicationContext-monitor-communication.xml"})
+@ContextConfiguration(locations = { "classpath:/applicationContext-monitor-communication.xml" })
 public class EmailTest {
-//	@Autowired
+	@Autowired
 	private EmailServiceImpl simpleMail;
-	@Test
-	 public void testSendMail() {
-		 List<String> toList=new ArrayList<String>();
-		 toList.add("yekai@163.com");
-
-		 String[] strings = new String[toList.size()];
-
-		 toList.toArray(strings);
-		 simpleMail.sendMail("Spring SMTP Mail Subject123", "Spring SMTP Mail Text12345", strings);
-		 //mail.sendMail("标题", "内容", "收件人邮箱");
-	    }
-	public EmailServiceImpl getSimpleMail() {
-		return simpleMail;
-	}
-	public void setSimpleMail(EmailServiceImpl simpleMail) {
-		this.simpleMail = simpleMail;
-	}
 	
+	@Test
+	public void testSendMail() {
+//		List<String> toList = new ArrayList<String>();
+//		toList.add("yekai@163.com");
+//
+//		String[] strings = new String[toList.size()];
+//
+//		toList.toArray(strings);
+//		simpleMail.sendMail("Spring SMTP Mail Subject123",
+//				"Spring SMTP Mail Text12345", strings);
+	}
 }
--- monitor-server/src/test/java/com/moreopen/monitor/sms/SMSTest.java
+++ monitor-server/src/test/java/com/moreopen/monitor/sms/SMSTest.java
@@ -1,7 +1,5 @@
 package com.moreopen.monitor.sms;
 
-import java.util.NoSuchElementException;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,27 +7,21 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.moreopen.monitor.server.service.alarm.SMSServiceImpl;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:/applicationContext-monitor-communication.xml"})
 public class SMSTest {
+	
 	@Autowired
 	private SMSServiceImpl smsService;
+	
 	@Test
 	 public void testSendMail() {
 		
-		try {
-			smsService.sendSms("13671880451", "test");
-		} catch (NoSuchElementException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		 //mail.sendMail("标题", "内容", "收件人邮箱");
-	    }
+//		try {
+//			smsService.sendSms("13671880451", "test");
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//	    }
+	}
 }
